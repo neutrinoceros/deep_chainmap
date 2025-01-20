e8fbddf7e00e45a2abd2f12a16ea358759206503
Clément Robert <cr52@protonmail.com>
2025-01-20 17:11:56 +0100

    MNT: move to src layout, enable mypy's strict mode

--- src/deep_chainmap/__init__.py
+++ src/deep_chainmap/__init__.py
@@ -1,10 +1,11 @@
 from collections import ChainMap
 from collections.abc import Mapping
+from typing import Any
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
-def _depth_first_update(target: dict, source: Mapping) -> None:
+def _depth_first_update(target: dict[Any, Any], source: Mapping[Any, Any]) -> None:
     for key, val in source.items():
         if not isinstance(val, Mapping):
             target[key] = val
@@ -15,10 +16,10 @@ def _depth_first_update(target: dict, source: Mapping) -> None:
         _depth_first_update(target[key], val)
 
 
-class DeepChainMap(ChainMap):
+class DeepChainMap(ChainMap[Any, Any]):
     """A recursive subclass of ChainMap"""
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         submaps = [mapping for mapping in self.maps if key in mapping]
         if not submaps:
             return self.__missing__(key)
@@ -26,8 +27,8 @@ class DeepChainMap(ChainMap):
             return DeepChainMap(*(submap[key] for submap in submaps))
         return super().__getitem__(key)
 
-    def to_dict(self) -> dict:
-        d = {}
+    def to_dict(self) -> dict[Any, Any]:
+        d: dict[Any, Any] = {}
         for mapping in reversed(self.maps):
             _depth_first_update(d, mapping)
         return d
