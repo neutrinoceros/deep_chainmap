353767f9511b6680075674d27c9ba3b46a7b2e21
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2025-04-30 10:46:00 +0200

    Improve typehinting

--- src/deep_chainmap/__init__.py
+++ src/deep_chainmap/__init__.py
@@ -1,11 +1,14 @@
 from collections import ChainMap
 from collections.abc import Mapping
-from typing import Any
+from typing import TypeVar, Generic
 
 __version__ = "0.1.2"
 
+K = TypeVar("K")
+V = TypeVar("V")
 
-def _depth_first_update(target: dict[Any, Any], source: Mapping[Any, Any]) -> None:
+
+def _depth_first_update(target: dict[K, V], source: Mapping[K, V]) -> None:
     for key, val in source.items():
         if not isinstance(val, Mapping):
             target[key] = val
@@ -16,10 +19,10 @@ def _depth_first_update(target: dict[Any, Any], source: Mapping[Any, Any]) -> No
         _depth_first_update(target[key], val)
 
 
-class DeepChainMap(ChainMap[Any, Any]):
+class DeepChainMap(Generic[K, V], ChainMap[K, V]):
     """A recursive subclass of ChainMap"""
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: K) -> V:
         submaps = [mapping for mapping in self.maps if key in mapping]
         if not submaps:
             return self.__missing__(key)
@@ -27,8 +30,8 @@ class DeepChainMap(ChainMap[Any, Any]):
             return DeepChainMap(*(submap[key] for submap in submaps))
         return super().__getitem__(key)
 
-    def to_dict(self) -> dict[Any, Any]:
-        d: dict[Any, Any] = {}
+    def to_dict(self) -> dict[K, V]:
+        d: dict[K, V] = {}
         for mapping in reversed(self.maps):
             _depth_first_update(d, mapping)
         return d
